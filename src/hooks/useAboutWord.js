abb32a06fc4f2da55b2b459ef6fdd98f43e7d295
Dayana Maia <dayrmaia@gmail.com>
2020-04-01 20:12:22 -0300

    fetch data about word

---
+++ src/hooks/useAboutWord.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+const useAboutWord = (word) => {
+    const [infoWord, setinfoWord] = useState({});
+
+    useEffect(() => {
+        const urlAPI = `https://api.pearson.com/v2/dictionaries/brep/entries?headword=${word}`;
+        const item = window.localStorage.getItem('list');
+
+        async function fecthData() {
+
+            if(item) {
+                //setListWords([JSON.parse(item)]);
+            } else {
+                try{
+                    const data = await fetch(urlAPI);
+                    const json = await data.json();
+
+                    if(json) {
+                        setinfoWord(json.results[0]);
+                    }
+
+                } catch(error) {
+                    console.log(error);
+                }
+            }
+        }
+
+        fecthData();
+    },[word]);
+
+    return infoWord;
+}
+
+export default useAboutWord;
--- src/pages/AboutWord/AboutWord.js
+++ src/pages/AboutWord/AboutWord.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useAboutWord from '../../hooks/useAboutWord';
 
 const AboutWord = () => {
+    const { word } = useParams();
+    
+    const { 
+        headword, 
+        part_of_speech,
+        pronunciations: [{ipa, lang}] = [{ipa: '', lang: ''}],
+        senses
+    } = useAboutWord(word);
+
     return(
         <>
-            <h2>AboutWord</h2>
+            <div className="modal-word">
+                <div className="btn-close"><span title="fechar">X</span></div>
+                <div className="modal-container">
+                    <div className="modal-content">
+                        <p>
+                            <span className="category">palavra</span>
+                            <span className="word">{headword}</span>
+                            <span className="pronounce"> - {ipa}</span>
+                            <span> {part_of_speech} - {lang}</span>
+                        </p>
+                        {senses && senses[0].translations &&
+                        <>
+                            <div className="box-translate">
+                                <p className="title-translate">
+                                    {senses[0].translations[0].example[0].text}
+                                    <span>{senses[0].translations[0].example[0].translation.text}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    <span className="category">tradução</span>
+                                    {senses[0].translations[0].text}
+                                </p>
+                            </div>
+                        </>
+                        }
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
